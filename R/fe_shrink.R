0c41f6255508a0bfab56b49067c30c5626e4a667
Soonwoo Kwon <k.soon.u@gmail.com>
2020-09-16 12:07:52 -0400

    Leave only the init val part, and return URE value

--- R/fe_shrink.R
+++ R/fe_shrink.R
@@ -9,8 +9,8 @@
 ##' @param type 
 ##' @export
 fe_shrink <- function(y, M, centering = c("0", "gen", "cov"), W = NULL,
-                      type = c("URE", "EBMLE"), spg = FALSE,
-                      init_vals = 1) {
+                      type = c("URE", "EBMLE"), init_vals = 1) {
+  
   if(!is.matrix(y)) {
     warning("y is not a matrix, assuming T=1.")
     y <- matrix(y, nrow = 1)
@@ -33,61 +33,25 @@ fe_shrink <- function(y, M, centering = c("0", "gen", "cov"), W = NULL,
     } else {
       obj <- make_nll_obj(y, M, centering, spg = spg)
     }
+    
+    opt <- NULL
 
-    if (!spg) {
-
-      opt <- NULL
-
-      for(j in 1:init_vals) {
-        if (j == 1) {
-          init_val <- rep(1, T*(T+1)/2)
-        } else {
-          init_val <- j * runif(T*(T+1)/2)
-        }
-
-        opt_res <- optim(par = rep(0, T*(T+1)/2), obj)
-        ## control=list(trace=TRUE))
-        opt <- min(opt_res$val, opt)
-
-        if (opt == opt_res$val) {
-          L <- opt_res$par
-          Lambda_opt <- make_from_lowertri(L, T)
-        }
-      }      
-    } else if (spg) {
-      if (type == "URE") {
-        ## obj_deriv <- NULL
-        obj_deriv <- make_URE_deriv(y, M)
+    for(j in 1:init_vals) {
+      if (j == 1) {
+        init_val <- .5 * rep(1, T*(T+1)/2)
       } else {
-        obj_deriv <- NULL
+        init_val <- .5 * j * rep(1, T*(T+1)/2)
       }
 
-      opt <- NULL
+      opt_res <- optim(par = rep(0, T*(T+1)/2), obj)
+      ## control=list(trace=TRUE))
+      opt <- min(opt_res$val, opt)
+
+      if (opt == opt_res$val) {
+        L <- opt_res$par
+        Lambda_opt <- make_from_lowertri(L, T)
+      }
       
-      for(j in 1:init_vals) {
-        if (j == 1) {
-          init_val <- rep(1, T^2)
-        } else {
-          init_val <- make_from_lowertri(j * rnorm(T*(T+1)/2), T)
-        }
-        
-        opt_res <- BB::spg(init_val, fn = obj,
-                           gr = obj_deriv,
-                           project = proj_spg,
-                           projectArgs = list(T = T),
-                           control = list(checkGrad = FALSE, trace = FALSE))
-        ## control=list(trace=TRUE))
-        opt <- min(opt_res$val, opt)
-
-        if (opt == opt_res$val) {
-          Lambda_opt <- opt_res$par
-          Lambda_opt <- matrix(Lambda_opt, nrow = T)
-        }
-      }
-    }
-          
-    
-    
     for (j in 1:J) {
       thetahat[, j] <- thetahat_j(mu = 0, Lambda_opt, M[[j]], y[, j])
     }
